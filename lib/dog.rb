e1d2b53707eca238178bf229028b665996ee61c6
Rocio01 <zrmartinezg@gmail.com>
2020-09-08 16:26:41 -0500

    Add folders: bin, lib and made a test for the bot

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  [
+    1 => " Stray dogs in Russia have learned how to ride the complex
+            subway system, and get off at specific stops in search of food.",
+    2 => "Spiked collars were originally fashioned in ancient Greece to
+            protect dogs throats from wolf attacks.",
+    3 => 'Your Dog’s Sense of Smell is 1,000 to 10 Million Times Better Than Yours',
+    4 => 'Dogs Can Hear 4 Times as Far as Humans',
+    5 => 'Your Dog Can Smell Your Feelings',
+    6 => 'Dogs Only Have Sweat Glands in Their Paw',
+    7 => 'The African Hunting dog is the most successful hunter in the world',
+    8 => 'On average around 30% of Dalmatians are deaf in one ear and 5% are deaf in both. ',
+    9 => "The Beatles song 'A Day in the Life' has a frequency only dogs can hear.",
+    10 => 'Dogs noses are wet to help absorb scent chemicals'
+  ]
+end
