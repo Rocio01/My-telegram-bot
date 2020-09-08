813c22b7269a4e0ae605bd62ee348b0be14cc244
Rocio01 <zrmartinezg@gmail.com>
2020-09-08 17:48:21 -0500

    Add class Dog in dog.rb

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,24 +1,17 @@
 require 'telegram/bot'
+require_relative './dog.rb'
+
+
 
 class Bot
+attr_accessor :dog
   def initialize
     token = '1392241254:AAGQoWtD2GRs8TCG71nroHSDSAPtOOk58CQ'
-
+    @dog = Dog.new
     Telegram::Bot::Client.run(token) do |bot|
       bot.listen do |message|
-        text = [" Stray dogs in Russia have learned how to ride the complex
-                subway system, and get off at specific stops in search of food.",
-                "Spiked collars were originally fashioned in ancient Greece to
-                 protect dogs throats from wolf attacks.",
-                'Your Dog’s Sense of Smell is 1,000 to 10 Million Times Better Than Yours',
-                'Dogs Can Hear 4 Times as Far as Humans',
-                'Your Dog Can Smell Your Feelings',
-                'Dogs Only Have Sweat Glands in Their Paw',
-                'The African Hunting dog is the most successful hunter in the world',
-                'On average around 30% of Dalmatians are deaf in one ear and 5% are deaf in both. ',
-                "The Beatles song 'A Day in the Life' has a frequency only dogs can hear.",
-                'Dogs noses are wet to help absorb scent chemicals'].sample
-        bot.api.send_message(chat_id: message.chat.id, text: text)
+        dog_text = @dog.dog_curiosities.sample
+        bot.api.send_message(chat_id: message.chat.id, text: dog_text)
       end
     end
   end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,19 @@
 class Dog
-  [
-    1 => " Stray dogs in Russia have learned how to ride the complex
-            subway system, and get off at specific stops in search of food.",
-    2 => "Spiked collars were originally fashioned in ancient Greece to
-            protect dogs throats from wolf attacks.",
-    3 => 'Your Dog’s Sense of Smell is 1,000 to 10 Million Times Better Than Yours',
-    4 => 'Dogs Can Hear 4 Times as Far as Humans',
-    5 => 'Your Dog Can Smell Your Feelings',
-    6 => 'Dogs Only Have Sweat Glands in Their Paw',
-    7 => 'The African Hunting dog is the most successful hunter in the world',
-    8 => 'On average around 30% of Dalmatians are deaf in one ear and 5% are deaf in both. ',
-    9 => "The Beatles song 'A Day in the Life' has a frequency only dogs can hear.",
-    10 => 'Dogs noses are wet to help absorb scent chemicals'
-  ]
+  attr_reader :dog_curiosities
+  def initialize (dog_curiosities = nil)
+      @dog_curiosities = [" Stray dogs in Russia have learned how to ride the complex
+          subway system, and get off at specific stops in search of food.",
+          "Spiked collars were originally fashioned in ancient Greece to
+           protect dogs throats from wolf attacks.",
+          'Your Dog’s Sense of Smell is 1,000 to 10 Million Times Better Than Yours',
+          'Dogs Can Hear 4 Times as Far as Humans',
+          'Your Dog Can Smell Your Feelings',
+          'Dogs Only Have Sweat Glands in Their Paw',
+          'The African Hunting dog is the most successful hunter in the world',
+          'On average around 30% of Dalmatians are deaf in one ear and 5% are deaf in both. ',
+          "The Beatles song 'A Day in the Life' has a frequency only dogs can hear.",
+          'Dogs noses are wet to help absorb scent chemicals']
+
+  end
+  
 end
