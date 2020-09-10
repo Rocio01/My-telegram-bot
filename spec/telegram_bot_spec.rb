7a43c705510f741ec354e3634255f713bf14b1e8
Rocio01 <zrmartinezg@gmail.com>
2020-09-10 02:31:54 -0500

    Fix offenses in rubocop

--- spec/telegram_bot_spec.rb
+++ spec/telegram_bot_spec.rb
@@ -2,12 +2,12 @@ require 'telegram/bot'
 
 describe Telegram::Bot do
   it 'has a version number' do
-    expect((described_class::VERSION).nil?).to eql(false)
+    expect(described_class::VERSION.nil?).to eql(false)
   end
 end
 
 describe Telegram::Bot::Client do
   it 'has a client' do
-    expect((Telegram::Bot::Client).nil?).to eql(false)
+    expect(Telegram::Bot::Client.nil?).to eql(false)
   end
 end
