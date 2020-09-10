b748ce88f5624f5ffb1f47e71ca7c81bf070b55e
Rocio01 <zrmartinezg@gmail.com>
2020-09-09 21:02:45 -0500

    Add images folder and logic to select a random img

--- lib/bot.rb
+++ lib/bot.rb
@@ -4,12 +4,15 @@ require_relative './cat.rb'
 require_relative './greetings.rb'
 
 class Bot
-  attr_reader :dog, :cat
+  attr_reader :dog, :cat, :photo
   def initialize
     token = '1392241254:AAGQoWtD2GRs8TCG71nroHSDSAPtOOk58CQ'
     @dog = Dog.new
     @cat = Cat.new
     @greeting = Welcome.new
+    image_path = '../images/*'
+    images = Dir[image_path]
+    @photo = images
 
     Telegram::Bot::Client.run(token) do |bot|
       bot.listen do |message|
@@ -23,6 +26,10 @@ class Bot
           bot.api.send_message(chat_id: message.chat.id, text: cat_text)
         elsif message.text == '/end'
           bot.api.send_message(chat_id: message.chat.id, text: @greeting.bye.sample)
+        elsif message.text == '/photo'
+          bot.api.send_photo(chat_id: message.chat.id, photo: Faraday::UploadIO.new(photo.sample, 'image/jpeg'))
+        else
+          bot.api.send_message(chat_id: message.chat.id, text: @greeting.wrong_command)
         end
       end
     end
--- lib/greetings.rb
+++ lib/greetings.rb
@@ -1,14 +1,17 @@
 class Welcome
-  attr_reader :welcome, :bye
+  attr_reader :welcome, :bye, :wrong_command
 
-  def initialize(_welcome = nil, _bye = nil)
-    @welcome = "  Welcome to copito1_bot, \n
+  def initialize(_welcome = nil, _bye = nil, _wrong_command = nil)
+    @welcome = "  Welcome to copito1_bot  \n
         This is a bot that will fill your curiosity about our furry friends \n
         Are you curious about dogs? type or click  /dog \n
         Are you curious about cats? type or click  /cat  \n
+        Do you want to see some pictures? type or click /photo \n
         When your curiosity is satisfied type or click   /end"
 
     @bye = ['✨ bye ✨', '✨ adios ✨', '✨ tchau ✨',
             '✨ adieu ✨', '✨ tschüss ✨']
+
+    @wrong_command = "Sorry I don\'t understand you please type or click /start"
   end
 end
