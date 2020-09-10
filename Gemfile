189c86cdb29da1a33f245298a744be18ec0afeb9
Rocio01 <zrmartinezg@gmail.com>
2020-09-09 23:07:30 -0500

    Add gem colorize to gemfile

--- Gemfile
+++ Gemfile
@@ -6,3 +6,6 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 gem 'rubocop', '~>0.81.0'
 
 gem 'telegram-bot-ruby'
+
+# gem "colorize"
+gem 'colorize', '~> 0.8.1'
--- bin/main.rb
+++ bin/main.rb
@@ -1,8 +1,8 @@
 require_relative '../lib/bot.rb'
 require 'colorize'
 
-puts " "
-puts "Welcome please open your telegram account and search for copito1_bot".cyan
-puts " "
-puts " When you are ready to finish just press ctrl + c".green
+puts ' '
+puts 'Welcome please open your telegram account and search for copito1_bot'.cyan
+puts ' '
+puts ' When you are ready to finish just press ctrl + c'.green
 Bot.new.copito_bot
